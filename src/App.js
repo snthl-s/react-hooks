98a43ddb12f287e0e81d1594ee47838df484135e
senthil.s2 <senthil.s2@partner.samsung.com>
2019-05-18 09:14:22 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -26,19 +26,16 @@ const App = () => {
 
 	const deleteUser = id => {
 		setEditing(false)
-
 		setUsers(users.filter(user => user.id !== id))
 	}
 
 	const updateUser = (id, updatedUser) => {
 		setEditing(false)
-
 		setUsers(users.map(user => (user.id === id ? updatedUser : user)))
 	}
 
 	const editRow = user => {
 		setEditing(true)
-
 		setCurrentUser({ id: user.id, name: user.name, username: user.username })
 	}
 
